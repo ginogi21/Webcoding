8d895194dd57fceee6c9cfd78a047a7a86e8e810
Jinok Choi <ginogi21@gmail.com>
2022-10-12 21:15:48 +0900

    Email Validation Check추가

---
+++ Javascript/13.Email Validation Check/assets/js/main.js
@@ -0,0 +1,26 @@
+let form = document.querySelector('#form');
+let text = document.  querySelector('#text');
+let email = document.querySelector('#email');
+
+function validate(){
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+  if(email.value.match(pattern)){
+    form.classList.add('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = "Your Email Address is Valid";
+    text.style.color = '#4caf50';
+  }else{
+    form.classList.remove('valid');
+    form.classList.add('invalid');
+    text.innerHTML = "Please Enter Valid Email Address";
+    text.style.color = '#f44336';
+  }
+  if(email.value == ""){
+    form.classList.remove('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = "";
+  }
+}
+
+email.addEventListener('keyup',validate)
