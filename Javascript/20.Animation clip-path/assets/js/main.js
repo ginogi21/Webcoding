803ff159b35f493b946654cb969cbcda197d55a4
Jinok Choi <ginogi21@gmail.com>
2022-11-04 14:12:47 +0900

    Animation clip-path추가

---
+++ Javascript/20.Animation clip-path/assets/js/main.js
@@ -0,0 +1,8 @@
+const sec1 = document.querySelector('.sec1');
+const sec2 = document.querySelector('.sec2');
+
+window.addEventListener('scroll',function(){
+  let value = this.window.scrollY;
+  sec1.style.clipPath = "circle("+value * 1.15+"px at 0 0)";
+  sec2.style.clipPath = "circle("+value * 0.90+"px at 100% 100%)";  
+})
