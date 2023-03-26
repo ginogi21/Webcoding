908f671dbe001ec79c9721d41d4a3aef17c719b5
Jinok Choi <ginogi21@gmail.com>
2023-03-26 22:21:36 +0900

    add files

---
+++ Javascript/23.Animated login form/assets/js/main.js
@@ -0,0 +1,25 @@
+// Show hidden-password
+
+const showHiddenPass = (loginPass, loginEye) =>{
+  const input = document.getElementById(loginPass),
+        iconEye = document.getElementById(loginEye)
+
+  iconEye.addEventListener("click",()=>{
+    // Change password to text
+    if(input.type === "password"){
+      // Switch to text
+      input.type = 'text';
+      // Icon change
+      iconEye.classList.add('ri-eye-line');
+      iconEye.classList.remove('ri-eye-off-line');
+    }else{
+      // Chage to password
+      input.type = 'password';
+      // Icon change
+      iconEye.classList.remove('ri-eye-line');
+      iconEye.classList.add('ri-eye-off-line');
+    }
+  })
+};
+
+showHiddenPass('login-pass','login-eye');
