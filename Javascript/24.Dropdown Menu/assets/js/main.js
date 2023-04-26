136ee6d9b24f7078029647980e93cf4e701c34dc
Jinok Choi <ginogi21@gmail.com>
2023-04-26 20:19:04 +0900

    revise and new file

---
+++ Javascript/24.Dropdown Menu/assets/js/main.js
@@ -0,0 +1,6 @@
+const profile = document.querySelector(".profile");
+const menu = document.querySelector(".menu");
+
+profile.addEventListener('click',()=>{
+  menu.classList.toggle("active");  
+})
