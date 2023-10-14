e54ff41914c50fd043fab603ce561dbd8352ceab
Jinok Choi <ginogi21@gmail.com>
2023-10-14 21:04:25 +0900

    add files

---
+++ Javascript/52.How to attach and detach class Name/assets/js/main.js
@@ -0,0 +1,33 @@
+const prevBtn = document.querySelector('.prevbtn');
+const nextBtn = document.querySelector('.nextbtn');
+
+const liElement = document.querySelectorAll('li');
+const numLi = liElement.length;
+
+let liNumeber = 0;
+
+nextBtn.addEventListener('click', ()=>{
+  liElement.forEach((li)=>{
+    li.classList.remove('active');
+  })
+
+  liNumeber++;  
+
+  if(liNumeber > (numLi -1 )){
+    liNumeber = 0 ;
+  }
+  liElement[liNumeber].classList.add('active');
+})
+
+prevBtn.addEventListener('click', ()=>{
+  liElement.forEach((li)=>{
+    li.classList.remove('active');
+  })
+
+  liNumeber--;  
+
+  if(liNumeber < 0 ){
+    liNumeber = numLi -1 ;
+  }
+  liElement[liNumeber].classList.add('active');
+})
