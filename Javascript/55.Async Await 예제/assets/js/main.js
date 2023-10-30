7ecbde69c105bbcaf77e96ed7ea448eb964d2445
Jinok Choi <ginogi21@gmail.com>
2023-10-30 14:33:13 +0900

    add files

---
+++ Javascript/55.Async Await 예제/assets/js/main.js
@@ -0,0 +1,63 @@
+const shop = document.querySelector('.shop');
+
+let stocks = {
+  Fruits : ['strawberry', 'grapes', 'banana', 'apple'],
+  liquid : ['water', 'ice'],
+  holder : ['cone', 'cup', 'stick'],
+  toppings : ['chocolate', 'peanuts']  
+}
+
+let is_shop_open = true;
+
+function time(ms){
+  return new Promise((resolve,reject)=>{
+    if(is_shop_open){
+      setTimeout(resolve, ms);
+    }
+    else{
+      reject(console.log('shop is closed'));
+    }
+  })
+}
+
+async function kitchen(){
+
+  try{
+    await time(2000)
+    console.log(`${stocks.Fruits[0]} was selected`);
+    shop.innerHTML = `<li>${stocks.Fruits[0]} was selected</li>`;
+
+    await time(0)
+    console.log('start the production');
+    shop.innerHTML = `<li>start the production</li>`;
+
+
+    await time(2000)
+    console.log('cut the fruit');
+
+    await time(1000)
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+
+    await time(1000)
+    console.log('start the machine');
+
+    await time(2000)
+    console.log(`Ice cream placed on ${stocks.holder[0]}`);
+
+    await time(3000)
+    console.log(`${stocks.toppings[0]} was selected`);
+
+    await time(2)
+    console.log('serve ice cream')
+
+  } 
+  catch(error){
+    console.log('customer left')
+  }
+  finally{
+    console.log('day ended, shop is closed')
+  }
+}
+
+kitchen();
+
