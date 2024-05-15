6df618c55a51c0dfa0e91afa3c8193cfb34bd60d
Jinok Choi <ginogi21@gmail.com>
2024-05-15 19:46:48 +0900

    add files

---
+++ Javascript/78.3D Hover effect/assets/js/main.js
@@ -0,0 +1,7 @@
+let container = document.getElementById('container');
+
+for(let i= 50; i >= -50; i--){
+	let span = document.createElement('span');
+	span.style.setProperty('--i', i);
+	container.appendChild(span)
+}
