dee13ea37f9a61d05e95d265a51302f070a26096
Jinok Choi <ginogi21@gmail.com>
2024-06-21 16:05:22 +0900

    add files Javascript

---
+++ Javascript/89.Glowing Corner Hover Effects/assets/js/main.js
@@ -0,0 +1,11 @@
+let cards = document.querySelectorAll('.card');
+
+cards.forEach(card =>{
+	card.addEventListener('mousemove',(e)=>{
+		let x = e.pageX - card.offsetLeft;
+		let y = e.pageY - card.offsetTop;
+
+		card.style.setProperty('--x', x + 'px');
+		card.style.setProperty('--y', y + 'px');
+	})
+})
